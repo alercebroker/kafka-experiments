cd2566e8225cbaa2f69c20d93c57b567f8ac5555
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2020-11-11 19:24:56 -0300

    Working kafka infrastructure

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # kafka-experiments
+
+This repository contains code for creating and running all the infrastructure necessary to run the kafka experiments, as well as running all software needed for consuming, producing, getting metrics, simulating pipeline steps, etc.
+
+The idea of the experiment is to:
+
+1. Produce messages using the LSST Schema
+2. Consume these messages as a "dummy" step
+3. Produce again
+4. Repeat 2 and 3 with as many dummy steps as the real pipeline
+5. Meassure rate of messages produced to each "dummy" step topic
+
+The first (minimal) attempt will contain the following infrastructure:
+
+![Diagram](./diagram.png)
+
+Depending on the results of the experiments the infrastructure configuration may vary to have more and bigger instances for each step, more containers for each step and or isolating other components to increase performance.
--- terraform/main.tf
+++ terraform/main.tf
@@ -7,9 +7,48 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_vpc" "kafka" {
+  cidr_block = "108.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "kafka-vpc"
+  }
+}
+
+resource "aws_subnet" "kafka" {
+  vpc_id     = aws_vpc.kafka.id
+  cidr_block = "108.0.0.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "kafka-subnet"
+  }
+}
+
+
+# Internet Gateway
+resource "aws_internet_gateway" "kafka" {
+  vpc_id = aws_vpc.kafka.id
+}
+
+# Route table: attach Internet Gateway
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.kafka.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.kafka.id
+  }
+}
+
+# Route table association with public subnets
+resource "aws_route_table_association" "public-subnet" {
+  subnet_id      = aws_subnet.kafka.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.kafka.id
 
   ingress {
     description = "SSH from VPC"
@@ -20,9 +59,17 @@ resource "aws_security_group" "allow_ssh" {
   }
 
   ingress {
-    description = "SSH from VPC"
+    description = "Zookeeper port"
     from_port   = 2181
     to_port     = 2181
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Kafka port"
+    from_port   = 9092
+    to_port     = 9092
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -41,8 +88,9 @@ resource "aws_security_group" "allow_ssh" {
 
 resource "aws_instance" "zookeeper" {
   ami                         = "ami-0941665311074d970"
-  instance_type               = "t2.micro"
+  instance_type               = "t2.medium"
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+  subnet_id                   = aws_subnet.kafka.id
   associate_public_ip_address = true
   key_name                    = "alerce"
 
@@ -61,11 +109,11 @@ resource "aws_instance" "zookeeper" {
 resource "aws_instance" "kafka" {
   count                       = 1
   ami                         = "ami-0876854ad385cfee0"
-  instance_type               = "t2.micro"
+  instance_type               = "t2.medium"
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+  subnet_id                   = aws_subnet.kafka.id
   associate_public_ip_address = true
   key_name                    = "alerce"
-  depends_on                  = [aws_instance.zookeeper]
 
   tags = {
     Name = "experiment-kafka"
@@ -79,11 +127,17 @@ resource "aws_instance" "kafka" {
   }
 
   provisioner "file" {
-    content     = templatefile("templates/server.properties", { zookeeper_host = aws_instance.zookeeper.private_ip, zookeeper_port = 2181, broker_id = count.index + 1})
+    content = templatefile("templates/server.properties", {
+      zookeeper_host  = aws_instance.zookeeper.private_ip,
+      zookeeper_port  = 2181,
+      broker_id       = count.index + 1,
+      kafka_public_ip = self.public_ip,
+      kafka_port      = 9092
+    })
     destination = "/tmp/server.properties"
   }
 
   provisioner "remote-exec" {
-    inline = ["sudo mv -f /tmp/server.properties /etc/kafka/config/server.properties"]
+    inline = ["sudo mv -f /tmp/server.properties /etc/kafka/config/server.properties", "sudo systemctl restart kafka"]
   }
 }
