d53b73ab6ff43f8189561c7da504c0fe3cc2162f
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2020-11-26 09:23:46 -0300

    added scaling variables and using instances with nvme

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,54 +12,59 @@ variable "simulator_scale" {
   default     = 1
 }
 
+variable "simulator_count" {
+  description = "Number of simulator instances"
+  default     = 1
+}
+
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "kafka" {
+resource "aws_vpc" "kafka_experiment" {
   cidr_block           = "108.0.0.0/16"
   enable_dns_hostnames = true
   tags = {
-    Name = "kafka-vpc"
+    Name = "kafka-experiment-vpc"
   }
 }
 
-resource "aws_subnet" "kafka" {
-  vpc_id                  = aws_vpc.kafka.id
+resource "aws_subnet" "kafka_experiment" {
+  vpc_id                  = aws_vpc.kafka_experiment.id
   cidr_block              = "108.0.0.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1a"
   tags = {
-    Name = "kafka-subnet"
+    Name = "kafka-experiment-subnet"
   }
 }
 
 
 # Internet Gateway
-resource "aws_internet_gateway" "kafka" {
-  vpc_id = aws_vpc.kafka.id
+resource "aws_internet_gateway" "kafka_experiment" {
+  vpc_id = aws_vpc.kafka_experiment.id
 }
 
 # Route table: attach Internet Gateway
 resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.kafka.id
+  vpc_id = aws_vpc.kafka_experiment.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.kafka.id
+    gateway_id = aws_internet_gateway.kafka_experiment.id
   }
 }
 
 # Route table association with public subnets
 resource "aws_route_table_association" "public-subnet" {
-  subnet_id      = aws_subnet.kafka.id
+  subnet_id      = aws_subnet.kafka_experiment.id
   route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.kafka.id
+  vpc_id      = aws_vpc.kafka_experiment.id
 
   ingress {
     description = "SSH from VPC"
@@ -131,10 +136,10 @@ resource "aws_security_group" "allow_ssh" {
 
 resource "aws_instance" "zookeeper" {
   ami                         = "ami-0698fe54008f8e70e"
-  instance_type               = "t2.small"
+  instance_type               = "m5a.large"
   availability_zone           = "us-east-1a"
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
-  subnet_id                   = aws_subnet.kafka.id
+  subnet_id                   = aws_subnet.kafka_experiment.id
   associate_public_ip_address = true
   key_name                    = "alerce"
 
@@ -155,10 +160,10 @@ resource "aws_instance" "zookeeper" {
 resource "aws_instance" "kafka" {
   count                       = 3
   ami                         = "ami-073f7717c8952cb06"
-  instance_type               = "t2.2xlarge"
+  instance_type               = "m5a.xlarge"
   availability_zone           = "us-east-1a"
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
-  subnet_id                   = aws_subnet.kafka.id
+  subnet_id                   = aws_subnet.kafka_experiment.id
   associate_public_ip_address = true
   key_name                    = "alerce"
 
@@ -189,12 +194,12 @@ resource "aws_instance" "kafka" {
       "sudo mkdir /mnt/data1",
       "sudo mkdir /mnt/data2",
       "sudo mkdir /mnt/data3",
-      "sudo mkfs.ext4 /dev/xvdb",
-      "sudo mount /dev/xvdb /mnt/data1",
-      "sudo mkfs.ext4 /dev/xvdc",
-      "sudo mount /dev/xvdc /mnt/data2",
-      "sudo mkfs.ext4 /dev/xvdd",
-      "sudo mount /dev/xvdd /mnt/data3",
+      "sudo mkfs.ext4 /dev/nvme1n1",
+      "sudo mount /dev/nvme1n1 /mnt/data1",
+      "sudo mkfs.ext4 /dev/nvme2n1",
+      "sudo mount /dev/nvme2n1 /mnt/data2",
+      "sudo mkfs.ext4 /dev/nvme3n1",
+      "sudo mount /dev/nvme3n1 /mnt/data3",
       "sudo mkdir /mnt/data1/kafka",
       "sudo mkdir /mnt/data2/kafka",
       "sudo mkdir /mnt/data3/kafka",
@@ -219,7 +224,7 @@ resource "aws_instance" "prometheus" {
   instance_type               = "t2.small"
   availability_zone           = "us-east-1a"
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
-  subnet_id                   = aws_subnet.kafka.id
+  subnet_id                   = aws_subnet.kafka_experiment.id
   associate_public_ip_address = true
   key_name                    = "alerce"
 
@@ -239,9 +244,9 @@ resource "aws_instance" "prometheus" {
       kafka2_ip    = aws_instance.kafka[1].private_ip,
       kafka3_ip    = aws_instance.kafka[2].private_ip,
       zookeeper_ip = aws_instance.zookeeper.private_ip,
-      simulator_ip = aws_instance.simulator.private_ip,
-      # pipeline_ip  = aws_instance.pipeline.*.private_ip,
-      jmx_port = 7075
+      simulator_ip = aws_instance.simulator.*.private_ip,
+      pipeline_ip  = aws_instance.pipeline.*.private_ip,
+      jmx_port     = 7075
     })
     destination = "/tmp/prometheus.yml"
   }
@@ -255,11 +260,12 @@ resource "aws_instance" "prometheus" {
 }
 
 resource "aws_instance" "simulator" {
-  ami                         = "ami-0db4193204e4c2a59"
-  instance_type               = "c5a.4xlarge"
-  availability_zone           = "us-east-1a"
+  count         = var.simulator_count
+  ami           = "ami-0db4193204e4c2a59"
+  instance_type = "c5a.4xlarge"
+  # availability_zone           = "us-east-1a"
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
-  subnet_id                   = aws_subnet.kafka.id
+  subnet_id                   = aws_subnet.kafka_experiment.id
   associate_public_ip_address = true
   key_name                    = "alerce"
 
@@ -293,41 +299,41 @@ resource "aws_instance" "simulator" {
   }
 }
 
-# resource "aws_instance" "pipeline" {
-#   count                       = var.pipeline_scale
-#   ami                         = "ami-0db4193204e4c2a59"
-#   instance_type               = "c5a.4xlarge"
-#   availability_zone           = "us-east-1a"
-#   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
-#   subnet_id                   = aws_subnet.kafka.id
-#   associate_public_ip_address = true
-#   key_name                    = "alerce"
+resource "aws_instance" "pipeline" {
+  count                       = var.pipeline_scale
+  ami                         = "ami-0db4193204e4c2a59"
+  instance_type               = "c5a.4xlarge"
+  availability_zone           = "us-east-1a"
+  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+  subnet_id                   = aws_subnet.kafka_experiment.id
+  associate_public_ip_address = true
+  key_name                    = "alerce"
 
-#   tags = {
-#     Name = "kafka-experiment-pipeline-${count.index}"
-#   }
+  tags = {
+    Name = "kafka-experiment-pipeline-${count.index}"
+  }
 
-#   connection {
-#     type        = "ssh"
-#     host        = self.public_ip
-#     user        = "ubuntu"
-#     private_key = file(var.private_key_path)
-#   }
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file(var.private_key_path)
+  }
 
-#   provisioner "file" {
-#     content = templatefile("templates/dummy_step_docker_compose.yml", {
-#       kafka1_private_ip = aws_instance.kafka[0].private_ip,
-#       kafka2_private_ip = aws_instance.kafka[1].private_ip,
-#       kafka3_private_ip = aws_instance.kafka[2].private_ip,
-#       kafka_port        = 9092
-#     })
-#     destination = "/tmp/dummy_step_docker_compose.yml"
-#   }
-#   provisioner "remote-exec" {
-#     inline = [
-#       "sudo mv -f /tmp/dummy_step_docker_compose.yml /home/ubuntu/experiment/dummy_step/docker-compose.yml",
-#       "cd /home/ubuntu/experiment/dummy_step",
-#       "sudo docker-compose up -d",
-#     ]
-#   }
-# }
+  provisioner "file" {
+    content = templatefile("templates/dummy_step_docker_compose.yml", {
+      kafka1_private_ip = aws_instance.kafka[0].private_ip,
+      kafka2_private_ip = aws_instance.kafka[1].private_ip,
+      kafka3_private_ip = aws_instance.kafka[2].private_ip,
+      kafka_port        = 9092
+    })
+    destination = "/tmp/dummy_step_docker_compose.yml"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mv -f /tmp/dummy_step_docker_compose.yml /home/ubuntu/experiment/dummy_step/docker-compose.yml",
+      "cd /home/ubuntu/experiment/dummy_step",
+      "sudo docker-compose up -d",
+    ]
+  }
+}
